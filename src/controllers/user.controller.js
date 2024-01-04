f37140aae817ce1b96a6bf7c981f771782440d6e
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-01-04 19:04:40 +0530

    fix error inside logout controller function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -129,7 +129,7 @@ const loginUser = asyncHandler(async(req, res)=>{
 
 // logged Out
 const logoutUser = asyncHandler(async(req, res)=> {
-    await User.findByIdAndDelete(req.user._id, {
+    await User.findByIdAndUpdate(req.user._id, {
         $set:{refreshToken: undefined}
     },{new:true})
 
